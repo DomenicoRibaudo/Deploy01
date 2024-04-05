28560bf7f6754049d8d7842cddf8796c903fabb5
Domenico Ribaudo <domenicoribaudo06@gmail.com>
2024-04-05 13:13:52 +0200

    Initial commit

---
+++ src/main/java/com/develhope/Deploy01/controllers/DevNameController.java
@@ -0,0 +1,21 @@
+package com.develhope.Deploy01.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/devname")
+public class DevNameController {
+
+    @Value("${devName}")
+    private String devName;
+
+    @GetMapping
+    public ResponseEntity<String> getDevName() {
+        return ResponseEntity.ok("DevName:" + devName);
+    }
+
+}
